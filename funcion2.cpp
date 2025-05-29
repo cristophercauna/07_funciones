00722604c27b6b8f57f5c15cbf9a6e16e508f453
cristophercauna <ccaunam@unjbg.edu.pe>
2025-05-29 16:13:59 -0500

    Agregar a la funcion2.cpp: retorna 1 si es primo y 0 si no es primo

---
+++ funcion2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int verificar(int val);
+int main(){
+    int a;
+    cout<<"Ingrese el numero a verificar si es primo"; cin>>a;
+    if(verificar(a)==1){
+        cout<<"El numero es primo"<<endl;
+    }else{
+        cout<<"El numero NO es primo"<<endl;
+    }
+    return 0;
+}
+int verificar(int val){
+    int contador,i;
+    contador=0;
+    for(i=1 ; i<=val ; i++){
+        if(val%i==0){
+            contador=contador+1;
+        }
+    }
+    if(contador==2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
