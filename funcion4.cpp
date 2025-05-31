5f62b81ad67a8aee6e76a04f676739b11e7fc119
cristophercauna <ccaunam@unjbg.edu.pe>
2025-05-31 12:48:42 -0500

    Agregar a la funcion4.cpp: Una funcion que haga la suma de los primeros terminos n y lo imprima

--- funcion4.cpp
+++ funcion4.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+int sumafibonacci(int N){
+    int a,b,suma,i,siguientenum;
+    a=0;
+    b=1;
+    suma=0;
+    for(i=1;i<=N;i++){
+        suma=suma+a;
+    siguientenum=a+b;
+    a=b;
+    b=siguientenum;
+    }
+    return suma;
+    
+}
 void seriefibonacci (int N){
 
 	int a,b,i,siguientenum,suma;
@@ -18,10 +32,11 @@ void seriefibonacci (int N){
 	cout<<"La suma de la serie hasta el termino "<<N<<" es "<<suma<<endl;
 }
 int main(){
-	int n;
+	int n,suma;
 	cout<<"ingrese el valor de n: "; cin>>n;
 	cout<<"Serie Fibonacci: "<<endl;
 	seriefibonacci(n);
-	
+    suma=sumafibonacci(n);
+	cout<<"La suma de los "<<n<<" primeros terminos es "<<suma<<endl;
 	return 0;
 }
