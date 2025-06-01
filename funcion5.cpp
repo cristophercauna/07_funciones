1f52f12bca3e6d852b90ea2e98b6ad55f19b5272
cristophercauna <ccaunam@unjbg.edu.pe>
2025-06-01 11:02:41 -0500

    Agregar a la funcion5.cpp: Mostrar divisores propios y mensaje más claro para números perfectos

--- funcion5.cpp
+++ funcion5.cpp
@@ -4,21 +4,27 @@ void numenteroperfct(int N);
 int main(){
     int n;
     cout<<"ingrese el numero : "; cin>>n;
-    numenteroperfct(n);
+    if(n>0){
+        numenteroperfct(n);
+    }else{
+        cout<<"ingrese un numero positivo"<<endl;
+    }
     return 0;
 }
 void numenteroperfct (int N){
     int i,suma;
     suma=0;
+    cout<<"Divisores propios de "<< N << " son: ";
     for(i=1;i<N;i++){
         if(N%i==0){
+            cout<<i<<" "<<endl;
             suma=suma+i;
         }
     }
     if(suma==N){
-        cout<<"El numero si cumple con la regla"<<endl;
+        cout<<"El numero "<<N<<" Es un numero perfecto"<<endl;
     }
     else{
-        cout<<"no cumple"<<endl;
+        cout<<"El numero "<<N<<"NO es un numero perfecto"<<endl;
     }
 }
